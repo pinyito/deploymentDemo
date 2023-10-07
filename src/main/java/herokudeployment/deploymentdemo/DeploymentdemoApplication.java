cfcc5ee73eb2d8df277c5505c7f5678dd3e9a1c9
Eria Othieno Pinyi <pinyito@users.noreply.github.com>
2023-10-07 18:31:19 -0500

    intial commit

---
+++ src/main/java/herokudeployment/deploymentdemo/DeploymentdemoApplication.java
@@ -0,0 +1,13 @@
+package herokudeployment.deploymentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploymentdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploymentdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/herokudeployment/deploymentdemo/DeploymentdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package herokudeployment.deploymentdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploymentdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
